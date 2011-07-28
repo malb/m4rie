d267fc5a6d4776761b65fce084b26b11187e36b9
Martin Albrecht <martinralbrecht@googlemail.com>
2011-07-28 11:22:56 +0200

    revised benchmarketing code to be easier to build

--- bench/bench_elimination.cc
+++ bench/bench_elimination.cc
@@ -1,43 +1,67 @@
 #include <gf2e_cxx/finite_field_givaro.h>
 #include <m4rie/m4rie.h>
 #include <cpucycles.h>
-
-#include "walltime.h"
+#include "benchmarking.h"
 
 using namespace M4RIE;
 
-int main(int argc, char **argv) {
-  double wt = 0.0;
-  char *algorithm;
-  size_t r; 
+struct elim_params {
+  rci_t k; 
+  rci_t m;
+  rci_t n;
+  rci_t r;
+  char const *algorithm;  
+};
 
-  if (argc < 4)
-    m4ri_die("syntax: e m n alg");
-  int k = atoi(argv[1]);
-  int m = atoi(argv[2]);
-  int n = atoi(argv[3]);
-  if (argc == 5)
-    algorithm = argv[4];
-  else
-    algorithm = (char*)"default";
-  FiniteField *F = (FiniteField*)(new GFqDom<int>(2,k));
+int run(void *_p, unsigned long long *data, int *data_len) {
+  struct elim_params *p = (struct elim_params *)_p;
+  *data_len = 2;
+
+  FiniteField *F = (FiniteField*)(new GFqDom<int>(2,p->k));
   gf2e *ff = gf2e_init_givgfq(F);
 
-  mzed_t *A = mzed_init(ff,m,n);
+  mzed_t *A = mzed_init(ff,p->m,p->n);
   mzed_randomize(A);
 
-  wt = walltime(&wt);
-  unsigned long long t = cpucycles();
-  if(strcmp(algorithm,"travolta")==0)
-    r=  mzed_echelonize_travolta(A, 1);
-  else if(strcmp(algorithm,"naive")==0)
-    r = mzed_echelonize_naive(A, 1);
+  data[0] = walltime(0);
+  data[1] = cpucycles();
+
+  if(strcmp(p->algorithm,"travolta")==0)
+    p->r=  mzed_echelonize_travolta(A, 1);
+  else if(strcmp(p->algorithm,"naive")==0)
+    p->r = mzed_echelonize_naive(A, 1);
   else
-    r = mzed_echelonize(A, 1);
-  printf("m: %5d, n: %5d, r: %5zu, cpu cycles: %llu wall time: %lf\n",m, n, r, cpucycles() - t, walltime(&wt));
+    p->r = mzed_echelonize(A, 1);
+  data[1] = cpucycles() - data[1];
+  data[0] = walltime(data[0]);
 
   mzed_free(A);
-
   gf2e_free(ff);
   delete F;
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  global_options(&argc, &argv);
+
+  if (argc < 4)
+    m4ri_die("syntax: e m n alg\n");
+
+  struct elim_params params;
+
+  params.k = atoi(argv[1]);
+  params.m = atoi(argv[2]);
+  params.n = atoi(argv[3]);
+  if (argc == 5)
+    params.algorithm = argv[4];
+  else
+    params.algorithm = (char*)"default";
+
+  srandom(17);
+  unsigned long long data[2];
+  run_bench(run, (void*)&params, data, 2);
+
+  double cc_per_op = ((double)data[1])/ ( (double)params.m * (double)params.n * powl((double)params.r,0.807) );
+
+  printf("m: %5d, n: %5d, last r: %5d, cpu cycles: %10llu, cc/(mnr^0.807): %.5lf, wall time: %lf\n", params.m, params.n, params.r, data[1], cc_per_op, data[0] / 1000000.0);
 }
--- bench/bench_multiplication.cc
+++ bench/bench_multiplication.cc
@@ -1,44 +1,44 @@
 #include <gf2e_cxx/finite_field_givaro.h>
 #include <m4rie/m4rie.h>
-
-#include "cpucycles.h"
-#include "walltime.h"
+#include <cpucycles.h>
+#include "benchmarking.h"
 
 using namespace M4RIE;
 
-int main(int argc, char **argv) {
-  double wt = 0.0;
-  char *algorithm;
-  size_t r; 
 
-  if (argc < 4)
-    m4ri_die("syntax: e m n alg");
-  int k = atoi(argv[1]);
-  int m = atoi(argv[2]);
-  int n = atoi(argv[3]);
-  if (argc == 5)
-    algorithm = argv[4];
-  else
-    algorithm = (char*)"default";
-  FiniteField *F = (FiniteField*)(new GFqDom<int>(2,k));
+struct mul_params {
+  rci_t k; 
+  rci_t m;
+  rci_t n;
+  char const *algorithm;  
+};
+
+int run(void *_p, unsigned long long *data, int *data_len) {
+  struct mul_params *p = (struct mul_params *)_p;
+  *data_len = 2;
+
+  FiniteField *F = (FiniteField*)(new GFqDom<int>(2,p->k));
   gf2e *ff = gf2e_init_givgfq(F);
 
-  mzed_t *A = mzed_init(ff,m,n);
+  mzed_t *A = mzed_init(ff,p->m,p->n);
   mzed_randomize(A);
-  mzed_t *B = mzed_init(ff,n,m);
+  mzed_t *B = mzed_init(ff,p->n,p->m);
   mzed_randomize(B);
 
   mzed_t *C;
 
-  wt = walltime(&wt);
-  unsigned long long t = cpucycles();
-  if(strcmp(algorithm, "travolta")==0)
+  data[0] = walltime(0);
+  data[1] = cpucycles();
+
+  if(strcmp(p->algorithm, "travolta")==0)
     C = mzed_mul_travolta(NULL, A, B);
-  else if(strcmp(algorithm,"naive")==0)
+  else if(strcmp(p->algorithm,"naive")==0)
     C = mzed_mul_naive(NULL, A, B);
   else
     C = mzed_mul(NULL, A, B);
-  printf("k: %2d, m: %5d, n: %5d, cpu cycles: %15llu, wall time: %8.4lf s\n",k, m, n, cpucycles() - t, walltime(&wt));
+
+  data[1] = cpucycles() - data[1];
+  data[0] = walltime(data[0]);
 
   mzed_free(A);
   mzed_free(B);
@@ -46,4 +46,39 @@ int main(int argc, char **argv) {
 
   gf2e_free(ff);
   delete F;
+  return 0;
 }
+
+int main(int argc, char **argv) {
+  global_options(&argc, &argv);
+
+  if (argc < 4)
+    m4ri_die("syntax: e m n alg\n");
+
+  struct mul_params params;
+
+  params.k = atoi(argv[1]);
+  params.m = atoi(argv[2]);
+  params.n = atoi(argv[3]);
+  if (argc == 5)
+    params.algorithm = argv[4];
+  else
+    params.algorithm = (char*)"default";
+
+  srandom(17);
+  unsigned long long data[2];
+  run_bench(run, (void*)&params, data, 2);
+
+  /* yes this is incorrect but okay-ish for random matrices */
+  rci_t r;
+  if (params.n<params.m) 
+    r = params.n;
+  else
+    r = params.m;
+
+  double cc_per_op = ((double)data[1])/ ( (double)params.m * (double)params.n * powl((double)r,0.807) );
+
+  printf("m: %5d, n: %5d, cpu cycles: %10llu, cc/(mnr^0.807): %.5lf, wall time: %lf\n", params.m, params.n, data[1], cc_per_op, data[0] / 1000000.0);
+}
+
+
